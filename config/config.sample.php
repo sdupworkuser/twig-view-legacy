56d6554c250ce254f74a963cdd6508ab0355783b
Snehil <sdupworkuser@gmail.com>
2024-02-16 19:16:45 +0530

    uploaded app files

---
+++ config/config.sample.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'WyriHaximus' => [
+        'TwigView' => [
+            'environment' => [
+                // Anything you would pass into \Twig\Environment to overwrite the default settings, see: http://twig.sensiolabs.org/doc/api.html#environment-options
+            ],
+            'markdown' => [
+                'engine' => 'engine', // See https://twig.symfony.com/doc/3.x/filters/markdown_to_html.html and then set to `new DefaultMarkdown()`
+            ],
+            'flags' => [
+                'potentialDangerous' => false,
+            ],
+        ],
+    ],
+];
